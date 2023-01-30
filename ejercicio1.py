4daa100fd96473e73461bce6e7ae0cf175b30854
aatondouna <aatondouna1@educacion.navarra.es>
2023-01-30 11:25:58 +0100

    Ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,15 @@
+import pygame
+pygame.init()
 
+ventana = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Ejercicio 1")
 
+jugando = True
+while jugando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            jugando = False
+
+ventana.fill((255, 255, 255))
+pygame.display.flip()
+pygame.time.Clock().tick(60)
