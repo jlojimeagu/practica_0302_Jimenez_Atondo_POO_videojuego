4fa618115bc22df7914d3404fe340275cf8d5095
aatondouna <aatondouna1@educacion.navarra.es>
2023-02-07 13:47:23 +0100

    velocidad

--- bloq_tom.py
+++ bloq_tom.py
@@ -1,2 +1,13 @@
 class bloque:
-    def
+    def __init__(self, x, y, puntos):
+
+        super().__init__()  # herencia de otra clase
+
+    # inicialización de otros objetos
+
+        self.image = pg.image.load(os.path.join('resources’, ‘images’, ‘[]')
+
+        self.rect = self.image.get_rect(x=x, y=y)
+
+    self.puntos = puntos
+
