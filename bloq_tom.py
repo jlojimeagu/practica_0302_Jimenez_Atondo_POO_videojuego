fddefc3f38018b3ec7d07c21209c59a70c6dfb09
juan miguel <jlojimeagu1@educacion.navarra.es>
2023-02-07 13:48:13 +0100

    todo

--- bloq_tom.py
+++ bloq_tom.py
@@ -1,2 +1,12 @@
+import pygame.image
 class bloque:
-    def
+    def __init__(self, pt_x, pt_y, imagen):
+        """
+        :param pt_x:punto horizontal/X de la ventana
+        :param pt_y:punto vertical/Y del la ventana
+        :param imagen: imagen del ladrillo entre ""
+        """
+        self.x = pt_x
+        self.y = pt_y
+        self.imagen = pygame.image.load(imagen)
+        self.imagenrect = pygame.Rect(self.x, self.y)
